183a2a79537e931ed29fa0261eae841d9d56676c
Nguyen Bao Tin <legendarystudent2402@gmail.com>
2022-10-14 22:01:52 +0700

    utils.py: Hard-coding -> Soft-coding

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-cd source/
-python main.py level_1 ../input/level_1/map1.txt
-python main.py level_1 ../input/level_1/map2.txt
-python main.py level_1 ../input/level_1/map3.txt
+cd source/level_1/
+python main.py level_1 astar ../../input/level_1/map1.txt
+python main.py level_1 astar ../../input/level_1/map2.txt
+python main.py level_1 astar ../../input/level_1/map3.txt
