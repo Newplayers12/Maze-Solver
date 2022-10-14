496e7327592ad7e872cf5b3c15ccede90982e249
Nguyen Bao Tin <legendarystudent2402@gmail.com>
2022-10-14 22:03:08 +0700

    utils.py: Hard-coding -> Soft-coding

---
+++ source/level_1/main.py
@@ -0,0 +1,25 @@
+from bfs import *
+from dfs import *
+from A_star import *
+import sys
+
+if __name__ == '__main__':
+    
+    if len(sys.argv) != 4:
+        sys.exit("Usage: python main.py dfs level_1 map1.txt")
+
+    maze = None
+    if sys.argv[2] == "dfs":
+        print('Test Level 1: DFS - Search Algorithms...')
+        maze = DFS_Maze(sys.argv[3])
+        maze.dfs_Search()
+    elif sys.argv[2] == "bfs":
+        print('Test Level 1: BFS - Search Algorithms...')
+        maze = BFS_Maze(sys.argv[3])
+        maze.bfs_Search()
+    elif sys.argv[2] == "astar":
+        print('Test Level 1: A* Search Algorithms...')
+        maze = A_star_Maze(sys.argv[3])
+        maze.A_star()
+    
+    maze.visualize_maze(True, sys.argv[3])
---
+++ source/level_1/utils.py
@@ -0,0 +1,160 @@
+import sys
+import math
+from queue import PriorityQueue
+from queue import LifoQueue
+from queue import Queue
+import matplotlib.pyplot as plt
+import os
+
+
+
+def manhattan(Start, Goal):
+    return abs(Start[0] - Goal[0]) + abs(Start[1] - Goal[1])
+
+def euclidean(Start, Goal):
+    dx = Goal[0] - Start[0]
+    dy = Goal[1] - Start[1]
+    return math.sqrt(dx*dx + dy * dy)
+
+def diagonal(Start, Goal):
+    cost_n = 1 # cost of non-diagonal movement
+    cost_d = cost_n * math.sqrt(2) # cost of diagonal movement
+    d_max = max(abs(Goal[0] - Start[0]), abs(Goal[1] - Start[1]))
+    d_min = min(abs(Goal[0] - Start[0]), abs(Goal[1] - Start[1]))
+    return  cost_n * (d_max - d_min) + cost_d * d_min
+
+F = {
+    'manhattan': manhattan,
+    'euclidean': euclidean,
+    'diagonal': diagonal,
+}
+
+
+class Node():
+    def __init__(self, state, parent, action):
+        self.state = state
+        self.parent = parent
+        self.action = action
+    
+
+
+class Maze():
+    def __init__(self, file_name):
+        f=open(file_name,'r')
+        n_bonus_points = int(next(f)[:-1])
+        self.bonus_points = []
+        for i in range(n_bonus_points):
+            x, y, reward = map(int, next(f)[:-1].split(' '))
+            self.bonus_points.append((x, y, reward))
+
+        text=f.read()
+        self.matrix=[list(i) for i in text.splitlines()]
+        f.close()
+        self.shape = [len(self.matrix), len(self.matrix[0])]
+
+        self.start = None
+        self.goal = None
+        self.walls = []
+
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[0])):
+                if self.matrix[i][j]=='S':
+                    self.start=(i,j)
+
+                elif self.matrix[i][j]==' ':
+                    if (i==0) or (i==len(self.matrix)-1) or (j==0) or (j==len(self.matrix[0])-1):
+                        self.goal=(i,j)
+                
+                elif self.matrix[i][j] == 'X':
+                    self.walls.append((i, j))
+                        
+                else:
+                    pass
+    
+    def generateSuccessors(self, state):
+        row, col = state
+        candidates = [
+            ("up", (row - 1, col)),
+            ("down", (row + 1, col)),
+            ("left", (row, col - 1)),
+            ("right", (row, col + 1))
+        ]
+
+        result = []
+        for action, (r, c) in candidates:
+            if 0 <= r < self.shape[0] and 0 <= c < self.shape[1] and (r, c) not in self.walls:
+                result.append((action, (r, c)))
+        return result
+    
+    def visualize_maze(self, save_img = False, input_dir = None):
+        """
+        Args:
+        1. matrix: The matrix read from the input file,
+        2. bonus: The array of bonus points,
+        3. start, end: The starting and ending points,
+        4. route: The route from the starting point to the ending one, defined by an array of (x, y), e.g. route = [(1, 2), (1, 3), (1, 4)]
+        """
+        #1. Define walls and array of direction based on the route
+        walls = self.walls
+        route = self.solution[1]
+
+        if route:
+            direction=[]
+            for i in range(1,len(route)):
+                if route[i][0]-route[i-1][0]>0:
+                    direction.append('v') #^
+                elif route[i][0]-route[i-1][0]<0:
+                    direction.append('^') #v        
+                elif route[i][1]-route[i-1][1]>0:
+                    direction.append('>')
+                else:
+                    direction.append('<')
+
+            direction.pop(0)
+
+        #2. Drawing the map
+        ax=plt.figure(dpi=100).add_subplot(111)
+
+        for i in ['top','bottom','right','left']:
+            ax.spines[i].set_visible(False)
+
+        plt.scatter([i[1] for i in walls],[-i[0] for i in walls],
+                    marker='X',s=100,color='black')
+        
+        plt.scatter([i[1] for i in self.bonus_points],[-i[0] for i in self.bonus_points],
+                    marker='P',s=100,color='green')
+
+        plt.scatter(self.start[1],-self.start[0],marker='*',
+                    s=100,color='gold')
+
+        if route:
+            for i in range(len(route)-2):
+                plt.scatter(route[i+1][1],-route[i+1][0],
+                            marker=direction[i],color='silver')
+
+        plt.text(self.goal[1],-self.goal[0],'EXIT',color='red',
+            horizontalalignment='center',
+            verticalalignment='center')
+        plt.xticks([])
+        plt.yticks([])
+        
+        # Save Plotting Image
+        if save_img:
+            dir_info = input_dir.split('/')       
+            output_dir = os.path.join(os.path.pardir, os.path.pardir, 'output')
+
+            if not os.path.exists(output_dir):
+                os.mkdir(output_dir)
+            
+            output_dir = os.path.join(output_dir, dir_info[-2])
+            if not os.path.exists(output_dir):
+                os.mkdir(output_dir)
+            plt.savefig(os.path.join(output_dir, dir_info[-1].split('.')[-2] + '.png'))
+        
+        plt.show()
+
+        print(f'Starting point (x, y) = {self.start[0], self.start[1]}')
+        print(f'Ending point (x, y) = {self.goal[0], self.goal[1]}')
+        
+        for _, point in enumerate(self.bonus_points):
+            print(f'Bonus point at position (x, y) = {point[0], point[1]} with point {point[2]}')
