6fade246a98f57c29fe553590803e5fce903f463
leanhuynh <leanhuynh2002@gmail.com>
2022-10-18 15:52:24 +0700

    add map for level_1, level_2, level_3

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 cd source/level_1/
 
-# echo Doing the A\* algorithm
+echo Doing the A\* algorithm
 
-# python main.py level_1 astar ../../input/level_1/map1.txt
-# python main.py level_1 astar ../../input/level_1/map2.txt
-# python main.py level_1 astar ../../input/level_1/map3.txt
-# python main.py level_1 astar ../../input/level_1/map4.txt
-# python main.py level_1 astar ../../input/level_1/map5.txt
+python main.py level_1 astar ../../input/level_1/map1.txt
+python main.py level_1 astar ../../input/level_1/map2.txt
+python main.py level_1 astar ../../input/level_1/map3.txt
+python main.py level_1 astar ../../input/level_1/map4.txt
+python main.py level_1 astar ../../input/level_1/map5.txt
 
 # echo Doing the DFS algorithm
 
@@ -26,11 +26,11 @@ cd source/level_1/
 # python main.py level_1 ucs ../../input/level_1/map2.txt
 # python main.py level_1 ucs ../../input/level_1/map3.txt
 
-echo Doing the GBF Algorithm
+# echo Doing the GBF Algorithm
 
-python main.py level_1 gbf ../../input/level_1/map1.txt
-python main.py level_1 gbf ../../input/level_1/map2.txt
-python main.py level_1 gbf ../../input/level_1/map3.txt
+# python main.py level_1 gbf ../../input/level_1/map1.txt
+# python main.py level_1 gbf ../../input/level_1/map2.txt
+# python main.py level_1 gbf ../../input/level_1/map3.txt
 
 # echo Doing Level 2 maps
 
