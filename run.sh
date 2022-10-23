2bedcd2565779ea71f07685c24a5259c3d35957a
leanhuynh <leanhuynh2002@gmail.com>
2022-10-23 09:17:21 +0700

    changes

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ read -p "Press enter to start level 1"
 cd source/level_1/
 
 # echo Doing the DFS algorithm
-python main.py level_1 dfs ../../input/level_1/map0.txt
+# python main.py level_1 dfs ../../input/level_1/map0.txt
 # python main.py level_1 dfs ../../input/level_1/map1.txt
 # python main.py level_1 dfs ../../input/level_1/map2.txt
 # python main.py level_1 dfs ../../input/level_1/map3.txt
@@ -13,7 +13,7 @@ python main.py level_1 dfs ../../input/level_1/map0.txt
 
 
 # echo Doing the BFS algorithm
-python main.py level_1 bfs ../../input/level_1/map0.txt
+# python main.py level_1 bfs ../../input/level_1/map0.txt
 # python main.py level_1 bfs ../../input/level_1/map1.txt
 # python main.py level_1 bfs ../../input/level_1/map2.txt
 # python main.py level_1 bfs ../../input/level_1/map3.txt
@@ -23,7 +23,7 @@ python main.py level_1 bfs ../../input/level_1/map0.txt
 
 
 # echo Doing the UCS algorithm
-python main.py level_1 ucs ../../input/level_1/map0.txt
+# python main.py level_1 ucs ../../input/level_1/map0.txt
 # python main.py level_1 ucs ../../input/level_1/map1.txt
 # python main.py level_1 ucs ../../input/level_1/map2.txt
 # python main.py level_1 ucs ../../input/level_1/map3.txt
@@ -34,18 +34,6 @@ python main.py level_1 ucs ../../input/level_1/map0.txt
 
 echo Doing the GBF Algorithm with Manhattan
 python main.py level_1 gbfs ../../input/level_1/map0.txt 1
-<<<<<<< HEAD
-# python main.py level_1 gbfs ../../input/level_1/map1.txt 1
-# python main.py level_1 gbfs ../../input/level_1/map2.txt 1
-# python main.py level_1 gbfs ../../input/level_1/map3.txt 1
-# python main.py level_1 gbfs ../../input/level_1/map4.txt 1
-# python main.py level_1 gbfs ../../input/level_1/map5.txt 1
-# python main.py level_1 gbfs ../../input/level_1/map6.txt 1
-
-
-echo Doing the GBF Algorithm with Euclidean
-python main.py level_1 gbfs ../../input/level_1/map0.txt 2
-=======
 python main.py level_1 gbfs ../../input/level_1/map1.txt 1
 python main.py level_1 gbfs ../../input/level_1/map2.txt 1
 python main.py level_1 gbfs ../../input/level_1/map3.txt 1
@@ -56,7 +44,6 @@ python main.py level_1 gbfs ../../input/level_1/map6.txt 1
 
 # echo Doing the GBF Algorithm with Euclidean
 # python main.py level_1 gbfs ../../input/level_1/map0.txt 2
->>>>>>> fc32c43 (update gbfs)
 # python main.py level_1 gbfs ../../input/level_1/map1.txt 2
 # python main.py level_1 gbfs ../../input/level_1/map2.txt 2
 # python main.py level_1 gbfs ../../input/level_1/map3.txt 2
@@ -65,13 +52,8 @@ python main.py level_1 gbfs ../../input/level_1/map6.txt 1
 # python main.py level_1 gbfs ../../input/level_1/map6.txt 2
 
 
-<<<<<<< HEAD
-echo Doing the A\* algorithm with Manhattan
-python main.py level_1 astar ../../input/level_1/map0.txt 1
-=======
 # echo Doing the A\* algorithm with Manhattan
 # python main.py level_1 astar ../../input/level_1/map0.txt 1
->>>>>>> fc32c43 (update gbfs)
 # python main.py level_1 astar ../../input/level_1/map1.txt 1
 # python main.py level_1 astar ../../input/level_1/map2.txt 1
 # python main.py level_1 astar ../../input/level_1/map3.txt 1
@@ -79,13 +61,8 @@ python main.py level_1 astar ../../input/level_1/map0.txt 1
 # python main.py level_1 astar ../../input/level_1/map5.txt 1
 # python main.py level_1 astar ../../input/level_1/map6.txt 1
 
-<<<<<<< HEAD
-echo Doing the A\* algorithm with Euclidean
-python main.py level_1 astar ../../input/level_1/map0.txt 2
-=======
 # echo Doing the A\* algorithm with Euclidean
 # python main.py level_1 astar ../../input/level_1/map0.txt 2
->>>>>>> fc32c43 (update gbfs)
 # python main.py level_1 astar ../../input/level_1/map1.txt 2
 # python main.py level_1 astar ../../input/level_1/map2.txt 2
 # python main.py level_1 astar ../../input/level_1/map3.txt 2
